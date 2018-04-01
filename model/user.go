90e132e35ed30bda7889e5c8780d9ff02d542e89
Kevin Oh <kevin@nulab-inc.com>
2018-04-01 12:25:50 -0400

    Added id field to user model

--- model/user.go
+++ model/user.go
@@ -2,6 +2,7 @@ package model
 
 // User ... represents a single user
 type User struct {
+	ID       int    `db:"id"`
 	Name     string `db:"name"`
 	Email    string `db:"email"`
 	Password string `db:"password"`
